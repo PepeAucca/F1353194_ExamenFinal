6c54b880fe1e92ff667347ca66d2f9358a27de89
Braulio Accostupa Aucca <braulioaccos@gmail.com>
2023-05-22 17:25:18 -0500

    [ADD]segunda compilacion del .elf

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include "incFiles/stm32f103x6.h"
+#include "motorDriver/motorDriver.h"
+#include "serialUart/serialUart.h"
+
+int main(void)
+{
+	/*
+	Desarrollar el flujo principal del programa utilizando las librerias motorDriver y serialUart
+	*/
+	while(1);
+}
